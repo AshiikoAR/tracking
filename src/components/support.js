855ab10d0a5ddfb693c8d6c8a68d201c325a34e5
Ashiko <alexandre.raguin49@gmail.com>
2024-05-29 14:07:58 +0200

    modified:   src/components/support.js 	modified:   src/index.css

--- src/components/support.js
+++ src/components/support.js
@@ -7,7 +7,7 @@ function Support() {
                 <div>
                     <h2><span style={{color:"red", fontWeight:"bolder"}}>[HELP]</span> &bull; Contacter le service support</h2>
                 </div>
-                <div>
+                <div className="help">
                     <h3>RAGUIN Alexandre | Développeur - Alternant amélioration continue</h3>
                     <ul>
                         <li>&#128238; &bull; &ensp;
@@ -50,43 +50,47 @@ function Support() {
                 <div>
                     <h2><span style={{color:"red", fontWeight:"bolder"}}>[DOCS]</span> &bull; Documents</h2>
                 </div>
-                <div>
-                    <h3>RAGUIN Alexandre | Développeur - Alternant amélioration continue</h3>
-                    <ul>
-                        <li>&#128238; &bull; &ensp;
-                            <a href="mailto:a.raguin@daher.com?cc=r.delon@daher.com&subject=%5BTRACKING%5D%20-%20Demande%20de%20support">
-                                Par mail : a.raguin@daher.com
-                            </a>
-                        </li>
-                        <li>&#128222; &bull; &ensp;
-                            <a href="tel:+33617518909">
-                                Par Téléphone : +33 6 17 51 89 09
-                            </a>
-                        </li>
-                        <li>&#128241; &bull; &ensp;
-                            <a href="https://teams.microsoft.com/l/chat/0/0?users=a.raguin@daher.com">
-                                Via Microsoft Teams : Alexandre RAGUIN
-                            </a>
-                        </li>
-                    </ul>
-                    <h3>DELON Romain | Développeur - Ingénieur amélioration continue</h3>
-                    <ul>
-                        <li>&#128238; &bull; &ensp;
-                            <a href="mailto:r.delon@daher.com?cc=a.raguin@daher.com&subject=%5BTRACKING%5D%20-%20Demande%20de%20support">
-                                Par mail : r.delon@daher.com
-                            </a>
-                        </li>
-                        <li>&#128222; &bull; &ensp;
-                            <a href="tel:+33752641188">
-                                Par téléphone : +33 7 52 64 11 88
-                            </a>
-                        </li>
-                        <li>&#128241; &bull; &ensp;
-                            <a href="https://teams.microsoft.com/l/chat/0/0?users=r.delon@daher.com">
-                                Via Microsoft Teams : Romain DELON
-                            </a>
-                        </li>
-                    </ul>
+                <div className="documents">
+                    <div className="docs">
+                        <h3>Aide clients :</h3>
+                        <ul>
+                            <li>&#128238; &bull; &ensp;
+                                <a href="mailto:a.raguin@daher.com?cc=r.delon@daher.com&subject=%5BTRACKING%5D%20-%20Demande%20de%20support">
+                                    Par mail : a.raguin@daher.com
+                                </a>
+                            </li>
+                            <li>&#128222; &bull; &ensp;
+                                <a href="tel:+33617518909">
+                                    Par Téléphone : +33 6 17 51 89 09
+                                </a>
+                            </li>
+                            <li>&#128241; &bull; &ensp;
+                                <a href="https://teams.microsoft.com/l/chat/0/0?users=a.raguin@daher.com">
+                                    Via Microsoft Teams : Alexandre RAGUIN
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="docs">
+                    <h3>Documentation technique :</h3>
+                        <ul>
+                            <li>&#128238; &bull; &ensp;
+                                <a href="mailto:r.delon@daher.com?cc=a.raguin@daher.com&subject=%5BTRACKING%5D%20-%20Demande%20de%20support">
+                                    Par mail : r.delon@daher.com
+                                </a>
+                            </li>
+                            <li>&#128222; &bull; &ensp;
+                                <a href="tel:+33752641188">
+                                    Par téléphone : +33 7 52 64 11 88
+                                </a>
+                            </li>
+                            <li>&#128241; &bull; &ensp;
+                                <a href="https://teams.microsoft.com/l/chat/0/0?users=r.delon@daher.com">
+                                    Via Microsoft Teams : Romain DELON
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
