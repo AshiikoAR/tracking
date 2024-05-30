7096c7c3cd15730a5d41067d8d6c9d3635073692
Ashiko <alexandre.raguin49@gmail.com>
2024-05-30 10:08:01 +0200

    modified:   src/components/choix-act.js 	modified:   src/index.css

--- src/components/choix-act.js
+++ src/components/choix-act.js
@@ -9,42 +9,36 @@ function ChoixAct() {
                 </div>
                 <form>
                     <div>
-                        <b>&#128339; &bull; Plage horaire</b>
-                        <label htmlFor="filtre">
-                            <select id="filtre" name="filtre">
-                                <option value="none">--</option>
-                                <option value="">Quart matin</option>
-                                <option value="">Journée normale</option>
-                                <option value="">Quart soir</option>
-                                <option value="">Nuit</option>
-                                <option value="">Formation</option>
-                            </select>
-                        </label>
+                        <label htmlFor="filtre"><b>&#128339; &bull; Plage horaire</b></label>
+                        <select id="filtre" name="filtre">
+                            <option value="none">--</option>
+                            <option value="">Quart matin</option>
+                            <option value="">Journée normale</option>
+                            <option value="">Quart soir</option>
+                            <option value="">Nuit</option>
+                            <option value="">Formation</option>
+                        </select>
                     </div>
                     <div>
-                        <b>&#128736;&#65039; &bull; Activité</b>
-                        <label htmlFor="filtre">
-                            <select id="filtre" name="filtre">
-                                <option value="none">--</option>
-                                <option value="">Réception / Rangement / Expéditions</option>
-                                <option value="">Picking</option>
-                                <option value="">Consolidation</option>
-                                <option value="">Kardex</option>
-                                <option value="">Gestion ordonnancement</option>
-                                <option value="">Tamaris</option>
-                                <option value="">Quais distribution</option>
-                            </select>
-                        </label>
+                        <label htmlFor="filtre"><b>&#128736;&#65039; &bull; Activité</b></label>
+                        <select id="filtre" name="filtre">
+                            <option value="none">--</option>
+                            <option value="">Réception / Rangement / Expéditions</option>
+                            <option value="">Picking</option>
+                            <option value="">Consolidation</option>
+                            <option value="">Kardex</option>
+                            <option value="">Gestion ordonnancement</option>
+                            <option value="">Tamaris</option>
+                            <option value="">Quais distribution</option>
+                        </select>
                     </div>
                     <div>
-                        <b>&#9881;&#65039; &bull; Sous-activité</b>
-                        <label htmlFor="filtre">
-                            <select id="filtre" name="filtre">
-                                <option value="none">--</option>
-                            </select>
-                        </label>
+                        <label htmlFor="filtre"><b>&#9881;&#65039; &bull; Sous-activité</b></label>
+                        <select id="filtre" name="filtre">
+                            <option value="none">--</option>
+                        </select>
                     </div>
-                    <div>
+                    <div className="choix">
                         <input type="button" value="&#128190; Débuter la sous-activité" onClick={() => {}} />
                         <p>&#128161; * Saisir les informations via liste déroulante ou scanner de code-barre</p>
                     </div>
@@ -85,6 +79,10 @@ function ChoixAct() {
                         </tr>
                         <tr>
                             <th scope="row">TEST Test 4</th>
+                            <td>Quais distribution &bull; Layout</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">TEST Test 5</th>
                             <td>Quais distribution &bull; Layout</td>
                         </tr>
                     </tbody>
